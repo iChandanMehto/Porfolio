09ca9559455e177e29fa7f4381d858a25b5cbf0b
Chandan Mehto <mehtochandanbusiness@gmail.com>
2025-01-09 21:18:26 +0530

    config file changes.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
+import tailwindScrollbar from 'tailwind-scrollbar';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  
   theme: {
     extend: {
       animation: {
@@ -13,15 +14,20 @@ export default {
       },
       keyframes: {
         rotate: {
-          "0%": { transform: "rotate(0deg) scale(10)" },
-          "100%": { transform: "rotate(-360deg) scale(10)" },
+          "0%": { transform: "rotate(0deg) scale(1)" },
+          "100%": { transform: "rotate(-360deg) scale(1)" },
+        },
+        "infinite-scroll": {
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(-100%)" },
         },
       },
       fontFamily: {
         spaceMono: ['"Space Mono"', 'monospace'], 
-    },
+      },
     },
   },
-  // plugins: [ ],
-}
-
+  plugins: [
+    tailwindScrollbar,
+  ],
+};
